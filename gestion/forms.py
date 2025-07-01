16ee69c1c08117fafed3262c9054eb82fbc5a818
DilanOrozco <dilanorozco0410@gmail.com>
2025-07-01 16:20:21 -0500

    Combino cambios locales con cambios del remoto

--- gestion/forms.py
+++ gestion/forms.py
@@ -3,8 +3,36 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistroForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+    email = forms.EmailField(
+        required=True, 
+        label='Correo electrónico'
+    )
+    first_name = forms.CharField(
+        max_length=40, 
+        label='Nombre'
+    )
+    last_name = forms.CharField(
+        max_length=50, 
+        label='Apellido'
+    )
+    password1 = forms.CharField(
+        label='Contraseña',
+        widget=forms.PasswordInput,
+        help_text=''  # opcional: quita el texto de ayuda
+    )
+    password2 = forms.CharField(
+        label='Confirmar contraseña',
+        widget=forms.PasswordInput,
+        help_text=''  # opcional: quita el texto de ayuda
+    )
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Nombre de usuario',
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+            'email': 'Correo electrónico',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar contraseña',
+        }
